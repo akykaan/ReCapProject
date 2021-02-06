e17e04949443bbc47854f3244cb0b0e60d0bf123
kaan akyüz <kaanaky77@gmail.com>
2021-02-06 15:22:05 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,79 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			CarManager carManager = new CarManager(new InMemoryCarDal());
+
+			Console.WriteLine("-----GetAll-----");
+			foreach (var cars in carManager.GetAll())
+			{
+				Console.WriteLine(
+					"Id:"+cars.Id+" "+
+					"Yılı:"+cars.BrandId + " "+
+					"Color Id:"+ cars.ColorId + " "+
+					"Model Year:" + cars.ModelYear + " " +
+					"Daily Price:" + cars.DailyPrice + " " +
+					"Description:" + cars.Description);
+			}
+
+			Console.WriteLine("-----GetById-----");
+			foreach (var cars in carManager.GetById(2))
+			{
+				Console.WriteLine(
+					"Id:" + cars.Id + " " +
+					"Yılı:" + cars.BrandId + " " +
+					"Color Id:" + cars.ColorId + " " +
+					"Model Year:" + cars.ModelYear + " " +
+					"Daily Price:" + cars.DailyPrice + " " +
+					"Description:" + cars.Description);
+			}
+
+			carManager.Add(
+				new Car 
+				{ 
+					Id = 7,
+					BrandId = 77,
+					ColorId=777,
+					ModelYear = 1992, 
+					DailyPrice = 5123,
+					Description = "Kiralık"
+				});
+
+			Console.WriteLine("\nAdd sonrası cagırma\n");
+
+			foreach (var cars in carManager.GetAll())
+			{
+				Console.WriteLine(
+					"Id:" + cars.Id + " " +
+					"Yılı:" + cars.BrandId + " " +
+					"Color Id:" + cars.ColorId + " " +
+					"Model Year:" + cars.ModelYear + " " +
+					"Daily Price:" + cars.DailyPrice + " " +
+					"Description:" + cars.Description);
+			}
+
+			carManager.Delete(new Car { Id=7});
+
+			Console.WriteLine("\nEklenen veriyi tekrar sildik\nDelete sonrası cagırma\n");
+
+			foreach (var cars in carManager.GetAll())
+			{
+				Console.WriteLine(
+					"Id:" + cars.Id + " " +
+					"Yılı:" + cars.BrandId + " " +
+					"Color Id:" + cars.ColorId + " " +
+					"Model Year:" + cars.ModelYear + " " +
+					"Daily Price:" + cars.DailyPrice + " " +
+					"Description:" + cars.Description);
+			}
+
+		}
+	}
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,56 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+	public class InMemoryCarDal:ICarDal
+	{
+		List<Car> _cars;
+		public InMemoryCarDal()
+		{
+			//Oracle,sql server,postgresql,
+			_cars = new List<Car> {
+				new Car{Id=1,BrandId=11,ColorId=111,ModelYear=1994,DailyPrice=1000,Description="Kiralik"},
+				new Car{Id=2,BrandId=22,ColorId=222,ModelYear=1995,DailyPrice=2000,Description="Satılık"},
+				new Car{Id=3,BrandId=33,ColorId=333,ModelYear=1996,DailyPrice=3000,Description="Kiralik"},
+				new Car{Id=4,BrandId=44,ColorId=444,ModelYear=1997,DailyPrice=4000,Description="Kiralik"},
+				new Car{Id=5,BrandId=55,ColorId=555,ModelYear=1998,DailyPrice=5000,Description="Kiralik"}
+			};
+		}
+
+		public void Add(Car car)
+		{
+			_cars.Add(car);
+		}
+
+		public void Delete(Car car)
+		{
+			Car carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+			_cars.Remove(carToDelete);
+		}
+
+		public List<Car> GetAll()
+		{
+			return _cars;
+		}
+
+		public List<Car> GetById(int id)
+		{
+			return _cars.Where(c => c.Id == id).ToList();
+		}
+
+		public void Update(Car car)
+		{
+			Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+			carToUpdate.BrandId = car.BrandId;
+			carToUpdate.ColorId = car.ColorId;
+			carToUpdate.DailyPrice = car.DailyPrice;
+			carToUpdate.Description = car.Description;
+			carToUpdate.ModelYear = car.ModelYear;
+		}
+	}
+}
